45e6f6fe25f2da2940aeb7c531fef2b345ade44e
donelleharris <donelle.harris@gmail.com>
2020-08-19 15:39:08 -0500

    'Very Easy' challenges with Jasmine tests

---
+++ js/edabit-challenges.js
@@ -0,0 +1,69 @@
+// How Edabit Works challenge
+function hello() {
+    return "hello edabit.com";
+}
+
+// Return the Sum of Two Numbers challenge
+function addition(a, b) {
+    return a + b;
+}
+
+//  Convert Minutes into Seconds challenge
+function convert(minutes) {
+    return minutes * 60;
+}
+
+//  Return the Next Number from the Integer Passed challenge
+function addition2(num) {
+    return num + 1;
+}
+
+//  Area of a Triangle challenge
+function triArea(base, height) {
+    return base * height / 2;
+}
+
+//  Return Something to Me! challenge
+function giveMeSomething(a) {
+    return "something " + a;
+}
+
+//  Convert Hours into Seconds challenge
+function howManySeconds(hours) {
+    return hours * 60 * 60;
+}
+//  Return the First Element in an Array challenge
+function getFirstValue(arr) {
+    return arr[0];
+}
+
+//  Maximum Edge of a Triangle challenge
+function nextEdge(side1, side2) {
+    return (side1 + side2) - 1;
+}
+
+//  Find the Perimeter of a Rectangle challenge
+function findPerimeter(length, width) {
+    return (length * 2) + (width * 2)
+}
+
+//  Return the Reminder from Two Numbers challenge
+function remainder(x, y) {
+    return x % y;
+}
+
+//  Football Points challenge
+function footballPoints(wins, draws, losses) {
+    losses = 0
+    return wins * 3 + draws + losses;
+}
+
+//  the Farm Problem challenge
+function animals(chickens, cows, pigs) {
+    return chickens * 2 + cows * 4 + pigs * 4;
+}
+
+// Convert string to Number challenge
+function stringInt(str) {
+    return Number(str);
+}
---
+++ tests/edabit-challenges-tests.js
@@ -0,0 +1,165 @@
+describe('hello', function (){
+    it('should be a defined function', function () {
+        expect(hello()).toBeDefined();
+    });
+    it('should return the string "hello edabit.com"', function () {
+        expect(hello()).toBe("hello edabit.com");
+    });
+});
+describe('addition', function (){
+    it('should be a defined function', function () {
+        expect(addition()).toBeDefined();
+    });
+    it('should return the sum of two numbers', function () {
+        expect(addition(2, 3)).toBe(5);
+    });
+    it('should return the sum of two numbers', function () {
+        expect(addition(-3, -6)).toBe(-9);
+    });
+});
+describe('convert', function (){
+    it('should be a defined function', function () {
+        expect(convert()).toBeDefined();
+    });
+    it('should return the product of 6 & 60', function () {
+        expect(convert(6)).toBe(360);
+    });
+    it('should return the product of 4 & 60', function () {
+        expect(convert(4)).toBe(240);
+    });
+});
+describe('addition2', function (){
+    it('should be a defined function', function () {
+        expect(addition2()).toBeDefined();
+    });
+    it('should return the next highest number', function () {
+        expect(addition2(2)).toBe(3);
+    });
+    it('should return the next highest number', function () {
+        expect(addition2(-9)).toBe(-8);
+    });
+    it('should return the next highest number', function () {
+        expect(addition2(0)).toBe(1);
+    });
+});
+describe('triArea', function () {
+    it('should be a defined function', function () {
+        expect(triArea()).toBeDefined();
+    });
+    it('should return the area of a triangle with a base of 3 and a height of 2 ', function () {
+        expect(triArea(3, 2)).toBe(3);
+    });
+    it('should return the area of a triangle with a base of 5 and a height of 4 ', function () {
+        expect(triArea(5, 4)).toBe(10);
+    });
+});
+describe('giveMeSomething', function () {
+    it('should be a defined function', function () {
+        expect(giveMeSomething()).toBeDefined();
+    });
+    it('should return a string', function () {
+        expect(typeof giveMeSomething()).toBe("string");
+    });
+    it('should return the string "something is cooking"', function () {
+        expect(giveMeSomething("is cooking")).toBe("something is cooking")
+    });
+    it('should return the string "something is better than nothing"', function () {
+        expect(giveMeSomething("is better than nothing")).toBe("something is better than nothing")
+    });
+});
+
+describe('howManySeconds', function (){
+    it('should be a defined function', function () {
+        expect(howManySeconds()).toBeDefined();
+    });
+    it('should return the product of 6 x 60 x 60', function () {
+        expect(howManySeconds(2)).toBe(7200);
+    });
+    it('should return the product of 4 x 60 x 60', function () {
+        expect(howManySeconds(10)).toBe(36000);
+    });
+
+});
+
+describe('getFirstValue', function (){
+    it('should be a defined function', function () {
+        expect(getFirstValue()).toBeDefined();
+    });
+    it('should return the first element in an array', function () {
+        expect(getFirstValue([1, 2, 3])).toBe(1);
+    });
+    it('should return the first element in an array', function () {
+        expect(getFirstValue([80, 5, 100])).toBe(80);
+    });
+
+});
+describe('nextEdge', function () {
+    it('should be a defined function', function () {
+        expect(nextEdge()).toBeDefined();
+    });
+    it('should return the maximum range of a triangle\'s third edge', function () {
+        expect(nextEdge(5,4)).toBe(8);
+    });
+    it('should return the maximum range of a triangle\'s third edge', function () {
+        expect(nextEdge(7, 9)).toBe(15);
+    });
+});
+describe('findPerimeter', function () {
+    it('should be a defined function', function () {
+        expect(findPerimeter()).toBeDefined();
+    });
+    it('should take two parameters (length and width) and finds the perimeter of a rectangle', function () {
+        expect(findPerimeter(6, 7)).toBe(26);
+    });
+    it('should take two parameters (length and width) and finds the perimeter of a rectangle', function () {
+        expect(findPerimeter(20, 10)).toBe(60);
+    });
+});
+
+describe('remainder', function () {
+    it('should be a defined function', function () {
+        expect(remainder()).toBeDefined();
+    });
+    it('should take two parameters (length and width) and finds the perimeter of a rectangle', function () {
+        expect(remainder(7, 2)).toBe(1);
+    });
+    it('should take two parameters (length and width) and finds the perimeter of a rectangle', function () {
+        expect(remainder(3, 4)).toBe(3);
+    });
+    it('should take two parameters (length and width) and finds the perimeter of a rectangle', function () {
+        expect(remainder(-9, 45)).toBe(-9);
+    });
+});
+describe('footballPoints', function () {
+    it('should be a defined function', function () {
+        expect(footballPoints()).toBeDefined();
+    });
+    it('should take the number of wins, draws and losses and calculates the number of points a football team has obtained so far', function () {
+        expect(footballPoints(3, 4, 2)).toBe(13)
+    });
+    it('should take the number of wins, draws and losses and calculates the number of points a football team has obtained so far', function () {
+        expect(footballPoints(5, 0, 2)).toBe(15)
+    });
+});
+describe('animals', function () {
+    it('should be a defined function', function () {
+        expect(animals()).toBeDefined();
+    });
+    it('should return the total number of legs given 5 chickens, 2 cows and 8 pigs', function () {
+        expect(animals(5, 2, 8)).toBe(50)
+    });
+    it('should return the total number of legs given 3 chickens, 4 cows and 7 pigs', function () {
+        expect(animals(3, 4, 7)).toBe(50)
+    });
+});
+describe('stringInt', function () {
+    it('should be a defined function', function () {
+        expect(stringInt()).toBeDefined();
+    });
+    it('should return a number', function () {
+        expect(typeof stringInt()).toBe(`number`)
+    });
+    it('should return 4 when passed the string "4"', function () {
+        expect(stringInt("4")).toBe(4)
+    });
+});
